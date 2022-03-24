9912fd99db906a4ed09e6bde0867d1394344c1d8
Montaser Mahadi <nasor_1590@hotmail.com>
2022-03-24 18:19:00 +0200

    Create product.routes.js

---
+++ app/routes/product.routes.js
@@ -0,0 +1,25 @@
+const { authJwt } = require("../middleware");
+const productController = require("../controllers/product.controller");
+const controller = require("../controllers/user.controller");
+
+module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+
+   app.post(
+    "/api/product/create/",
+    [authJwt.verifyToken, authJwt.isAdmin],
+	productController.createProduct
+  );
+
+  app.get(
+    "/api/product/list/",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    productController.listProduct
+);}
